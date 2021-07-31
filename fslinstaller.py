55490e706882d142d2aaa8ac8505045194d60849
Paul McCarthy <pauldmccarthy@gmail.com>
2021-07-31 13:30:18 +0100

    RF: Prevent unlikely but possible infinite self-update loop

--- fslinstaller.py
+++ fslinstaller.py
@@ -1439,7 +1439,11 @@ def self_update(manifest, workdir, checksum):
                      'checksum! Skipping update.', WARNING)
             return
 
-    cmd = [sys.executable, tmpf] + sys.argv[1:]
+    # Don't try and update again - if for some
+    # reason the online manifest reports a newer
+    # version than what is available, we would
+    # otherwise enter into an infinite loop.
+    cmd = [sys.executable, tmpf] + sys.argv[1:] + ['--no_self_update']
     log.debug('Running new installer: %s', cmd)
     os.execv(sys.executable, cmd)
 
