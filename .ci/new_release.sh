c9e11eaa3de71a068f362a71dbe6f8f96b35e061
Paul McCarthy <pauldmccarthy@gmail.com>
2021-09-17 15:36:18 +0100

    CI: Separate deploy / update manifest steps into separate jobs, so latter can run in docker.

--- .ci/new_release.sh
+++ .ci/new_release.sh
@@ -4,8 +4,8 @@
 # every time a new tag is added to the fsl/conda/installer gitlab
 # repository.
 #
-# First we copy the installer into the deployment directory, denoted by the
-# $FSLINSTALLER_DEPLOY_DIRECTORY environment variable.
+# This simply involves copying the installer into the deployment directory,
+# denoted by the $FSLINSTALLER_DEPLOY_DIRECTORY environment variable.
 
 set -e
 
@@ -19,13 +19,3 @@ if [ "$scriptver" != "$CI_COMMIT_TAG" ]; then
 fi
 
 cp fslinstaller.py $FSLINSTALLER_DEPLOY_DIRECTORY/
-
-# Then we call the update_manifest.py script, which opens a merge request
-# on the fsl/conda/manifest repository, to update the latest available
-# installer version in the manifest.
-#
-# The update_manifest.py script uses functionality from fsl-ci-rules, so
-# we need to install that before running the script.
-python -m pip install --upgrade pip
-python -m pip install git+https://git.fmrib.ox.ac.uk/fsl/fsl-ci-rules.git
-python ./.ci/update_manifest.py
