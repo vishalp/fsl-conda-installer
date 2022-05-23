c14e6f2661ade95f07388d1a09d64c765bd3ee46
Paul McCarthy <pauldmccarthy@gmail.com>
2022-05-23 10:45:08 +0100

    MNT: version

--- fslinstaller.py
+++ fslinstaller.py
@@ -54,7 +54,7 @@ log = logging.getLogger(__name__)
 __absfile__ = op.abspath(__file__).rstrip('c')
 
 
-__version__ = '1.9.0'
+__version__ = '1.10.0'
 """Installer script version number. This must be updated
 whenever a new version of the installer script is released.
 """
