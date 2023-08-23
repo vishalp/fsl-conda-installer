63f35ce41c1cb379da20a4c471891b7239df5b23
Paul McCarthy <pauldmccarthy@gmail.com>
2023-08-23 17:23:58 +0100

    TEST: Mark test which will fail if run by root user

--- test/test_context.py
+++ test/test_context.py
@@ -13,6 +13,7 @@ import pytest
 import fsl.installer.fslinstaller as inst
 
 
+@pytest.mark.noroottest
 def test_Context_admin_password():
     sudo = tw.dedent("""
     #!/usr/bin/env bash
@@ -34,8 +35,10 @@ def test_Context_admin_password():
         with mock.patch.dict(os.environ, PATH=path), \
              mock.patch('getpass.getpass', return_value='password'):
 
-            os.mkdir('needs_admin',         mode=438) # 0o666
-            os.mkdir('does_not_need_admin', mode=511) # 0o777
+            os.mkdir('needs_admin')
+            os.mkdir('does_not_need_admin')
+            os.chmod('needs_admin',         438) # 0o666
+            os.chmod('does_not_need_admin', 511) # 0o777
 
             ctx = inst.Context(None, op.join('needs_admin', 'fsl'))
             assert ctx.admin_password == 'password'
