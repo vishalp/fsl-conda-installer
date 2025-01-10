b169e98dcbb52d8f21f38c90a90297957861ed0e
Paul McCarthy <pauldmccarthy@gmail.com>
2025-01-10 15:41:12 +0000

    CI: Only run wrapper tests under py>=39

--- .ci/unit_tests.sh
+++ .ci/unit_tests.sh
@@ -10,11 +10,17 @@ if python -c 'import sys; sys.exit(sys.version_info[:2] >= (3, 3))'; then
   touch fsl/__init__.py
 fi
 
-
 pip install --upgrade pip setuptools wheel
 pip install pytest coverage pytest-cov mock typing
 
 # some tests will fail if run as root
 chmod a+rwx .
-su -s /bin/bash nobody -c "pytest -m      noroottest  --cov-report= --cov-append"
-pytest -m "not noroottest"
+su -s /bin/bash nobody -c "pytest -m noroottest  --cov-report= --cov-append --ignore=test/test_create_remove_wrapper.py"
+
+# The createFSLWrapper/removeFSLWrapper scripts require Python >= 3.9
+if python -c 'import sys; sys.exit(sys.version_info[:2] < (3, 9))'; then
+  pytest -k test_create_remove_wrapper --cov-report= --cov-append
+fi
+
+# Run all other tests
+pytest -m "not noroottest" --ignore=test/test_create_remove_wrapper.py
